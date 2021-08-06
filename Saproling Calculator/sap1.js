24c55f0daa57958e4cf832c1192edb74f3095f87
spacescript <simonbengoehring@gmail.com>
2021-08-05 21:23:11 -0700

    Update sap1.js

--- Saproling Calculator/sap1.js
+++ Saproling Calculator/sap1.js
@@ -3,4 +3,14 @@
 var nestInput = document.getElementById("nest-input");
 var nestNumber = document.getElementById("nest-number");
 
-nestNumber.innerText = "12";
+if(nestInput.addEventListener){
+    nestInput.addEventListener("submit", nestUpdate(), false);
+} else if (nestInput.attchEvent){
+    nestInput.attachEvent('onsubmit', nestUpdate());
+}
+
+function nestUpdate(){
+    nestNumber = nestInput;
+}
+
+// nestNumber.innerText = "12";
