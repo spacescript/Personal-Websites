5b73ac9b45a5b42f5d1a7446412c43e563f3dd09
spacescript <simonbengoehring@gmail.com>
2022-04-04 10:59:24 -0700

    Apr 22 Prac Pack 1

--- Saproling Calculator/sap1.js
+++ Saproling Calculator/sap1.js
@@ -1,7 +1,8 @@
 //This is to set the nest number at the top of the forms
 
-var nestInput = document.getElementById("nest-input");
-var nestNumber = document.getElementById("nest-number");
+const nestInput = document.getElementById("nest-input");
+const nestNumber = document.getElementById("nest-number");
+const nestDisplay = document.getElementById('nestDisplay');
 
 if(nestInput.addEventListener){
     nestInput.addEventListener("submit", nestUpdate(), false);
