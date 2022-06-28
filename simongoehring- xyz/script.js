9c99a35d3441226376c1736b9e42c7953cd67e9d
spacescript <simonbengoehring@gmail.com>
2022-06-27 19:46:44 -0700

    Update script.js

--- simongoehring- xyz/script.js
+++ simongoehring- xyz/script.js
@@ -3,5 +3,23 @@ const script = () => {
     <div>script</div>
   )
 }
-
+SVGDefsElement()
 export default script
+
+class TypesOfFood extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Types of Food:</h1>
+        {/* Change code below this line */}
+        
+        {/* Change code above this line */}
+      </div>
+    );
+  }
+};
+
+// Change code below this line
